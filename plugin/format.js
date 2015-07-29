fe6e422bb1521bd97f33ab9eee3c1cb657e5ac16
Jim McAndrew <jim@loc8.us>
2015-07-29 14:24:43 -0600

    Add basic project

---
+++ plugin/format.js
@@ -0,0 +1,19 @@
+/**
+ * @description Runer for jsformater
+ * support nodejs and v8 interpretator
+ */
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+(function (contentPath) {
+  exec('/usr/bin/semistandard "' + contentPath + '" --format', function (e, so, se) {
+    fs.readFile(contentPath, {
+      encoding: 'utf8'
+    }, function (err, data) {
+      if (!err) {
+        console.log(data);
+      }
+    });
+  });
+}).apply(this, (typeof process === 'object' && process.argv.splice(3)));
+
