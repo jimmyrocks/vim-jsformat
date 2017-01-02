8cbe2ab675e23bb112881a3d39723fe9c02463d8
Jim McAndrew <jim@loc8.us>
2017-01-02 17:38:32 -0500

    Support for Python Autopep8

--- plugin/format.js
+++ plugin/format.js
@@ -5,13 +5,19 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-(function (contentPath) {
-  exec('/usr/bin/semistandard "' + contentPath + '" --fix', function (e, so, se) {
+(function(contentPath, options, formatFile, fileType) {
+  var cmd = '/usr/bin/semistandard "' + contentPath + '" --fix';
+  if (fileType === 'python') {
+    cmd = '/usr/local/bin/autopep8 --in-place "' + contentPath + '"';
+  }
+  exec(cmd, function(e, so, se) {
     fs.readFile(contentPath, {
       encoding: 'utf8'
-    }, function (err, data) {
+    }, function(err, data) {
       if (!err) {
         console.log(data.replace(/[\r\n]+$/g, ''));
+      } else {
+        console.log(err);
       }
     });
   });
